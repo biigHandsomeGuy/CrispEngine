6ffe7c414186cfee302146b4254981124f774e6e
帅朝 王 <2410111241@qq.com>
2025-05-19 16:06:04 +0800

    Add orthographic camera

--- Crisp/src/Crisp/Renderer/Shader.h
+++ Crisp/src/Crisp/Renderer/Shader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <glm/glm.hpp>
 
 namespace Crisp{
 
@@ -13,6 +14,7 @@ namespace Crisp{
 		void Bind();
 		void Unbind();
 
+		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 	private:
 		uint32_t m_RendererID;
 	};
